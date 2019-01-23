476ae0468e766732b628d02345a7e56635512741
liang <402284347@qq.com>
2019-01-24 00:16:36 +0800

    src/components/MusicList.vue文件中同时调用vuex的mutations过多，导致同步冲突，让vue Detected崩溃，所以把逻辑转移到vuex中

--- src/components/MusicList.vue
+++ src/components/MusicList.vue
@@ -17,7 +17,7 @@
       </el-table-column>
     </el-table>
     <p class="load-music"
-      v-show="loadMusic"
+      v-show="loadMusicEvent"
       @click="load">
       点击加载更多音乐
     </p>
@@ -29,28 +29,15 @@
 import { mapState, mapMutations } from 'vuex'
 import axios from 'axios'
 export default {
-  data () {
-    return {
-      loading: false,
-      loadMusic: false
-    }
-  },
   methods: {
     load () {
       const residueMusic = this.list.length - this.showListIndex
       console.log(`剩余数量:${residueMusic}`)
-      console.log(`剩余数量:${residueMusic < 100}`)
       if (residueMusic < 100) {
-        for (let i = 0 + this.showListIndex; i < (residueMusic + this.showListIndex); ++i) {
-          this.addTableList(this.list[i])
-        }
-        this.setShowListIndex(this.list.length)
-        this.loadMusic = false
+        this.loadInsufficient(residueMusic)
+        this.setLoadMusicEvent(false)
       } else {
-        this.setShowListIndex(this.showListIndex + 100)
-        for (let i = this.showListIndex - 100; i <= this.showListIndex; ++i) {
-          this.addTableList(this.list[i])
-        }
+        this.loadSufficient()
       }
       console.log(`当前显示音乐数量:${this.showListIndex}`)
     },
@@ -59,50 +46,22 @@ export default {
       this.addHistory(row.name)
     },
     axiosList () {
-      this.loading = true
+      this.setLoading(true)
       axios.get(this.apiAddress).then(response => {
-        console.log(`开头：${new Date()}`)
-        const musicNum = response.data.length - 1
-        console.log(`歌曲数量：${musicNum}`)
-        this.setList([])
-        console.log(`创建list：${new Date()}`)
-        for (let i = 1; i < response.data.length; i++) {
-          let data = {
-            id: i,
-            name: response.data[i]
-          }
-          this.addList(data)
-          this.loading = false
-        }
-        console.log(`创建TableList：${new Date()}`)
-        this.setTableList([])
-        if (musicNum < 100) {
-          for (let i = 0; i <= musicNum; ++i) {
-            this.addTableList(this.list[i])
-          }
-          this.setShowListIndex(musicNum)
-        } else {
-          for (let i = 0; i < 100; ++i) {
-            this.addTableList(this.list[i])
-          }
-          this.setShowListIndex(100)
-          this.loadMusic = true
-        }
-        console.log(`结束：${new Date()}`)
-        console.log(`当前显示音乐数量:${this.showListIndex}`)
+        console.log(`歌曲数量：${response.data.length}`)
+        this.loadInit(response.data)
       }).catch(function (error) {
         console.log(error)
       })
-      console.log(`axiosList结束：${new Date()}`)
     },
     ...mapMutations('music', [
       'setName',
-      'setList',
-      'addList',
       'addHistory',
-      'addTableList',
-      'setTableList',
-      'setShowListIndex'
+      'loadInsufficient',
+      'loadSufficient',
+      'loadInit',
+      'setLoading',
+      'setLoadMusicEvent'
     ])
   },
   computed: {
@@ -112,7 +71,9 @@ export default {
       'showListIndex',
       'tableList',
       'musicApi',
-      'apiAddress'
+      'apiAddress',
+      'loading',
+      'loadMusicEvent'
     ])
   }
 }
--- src/store.js
+++ src/store.js
@@ -13,7 +13,9 @@ const music = {
     name: '空',
     tableList: [],
     showListIndex: 0,
-    list: []
+    list: [],
+    loading: false,
+    loadMusicEvent: false
   },
   mutations: {
     addHistory (state, node) {
@@ -28,17 +30,55 @@ const music = {
     setList (state, list) {
       state.list = list
     },
-    addList (state, data) {
-      state.list.push(data)
-    },
     setShowListIndex (state, index) {
       state.showListIndex = index
     },
-    addTableList (state, data) {
-      state.tableList.push(data)
-    },
     setTableList (state, list) {
       state.tableList = list
+    },
+    loadInsufficient (state, residue) {
+      for (let i = 0 + state.showListIndex; i < (residue + state.showListIndex); ++i) {
+        state.tableList.push(state.list[i])
+      }
+      state.showListIndex = state.list.length
+    },
+    loadSufficient (state) {
+      state.showListIndex = state.showListIndex + 100
+      for (let i = state.showListIndex - 100; i <= state.showListIndex; ++i) {
+        state.tableList.push(state.list[i])
+      }
+    },
+    loadInit (state, responseData) {
+      state.list = []
+      const musicNum = responseData.length - 1
+      for (let i = 1; i < responseData.length; i++) {
+        let data = {
+          id: i,
+          name: responseData[i]
+        }
+        state.list.push(data)
+        state.loading = false
+      }
+      state.tableList = []
+      if (musicNum < 100) {
+        for (let i = 0; i <= musicNum; ++i) {
+          state.tableList.push(state.list[i])
+        }
+        state.showListIndex = musicNum
+      } else {
+        for (let i = 0; i < 100; ++i) {
+          state.tableList.push(state.list[i])
+        }
+        state.showListIndex = 100
+        state.loadMusicEvent = true
+      }
+      console.log(`当前显示音乐数量:${state.showListIndex}`)
+    },
+    setLoading (state, value) {
+      state.loading = value
+    },
+    setLoadMusicEvent (state, value) {
+      state.loadMusicEvent = value
     }
   },
   getters: {
